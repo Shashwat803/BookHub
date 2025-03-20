4b379784e6cf0e7aa30a10b67d43d010b9b8274c
Shashwat803 <shashwatmishra803@gmail.com>
2025-03-20 16:15:17 +0530

    removed console logs

--- server/src/configs/db.ts
+++ server/src/configs/db.ts
@@ -1,11 +1,6 @@
 import { Sequelize } from "sequelize";
 require('dotenv').config()
 
-console.log("DBNAME:", process.env.DBNAME);
-console.log("USERNAME:", process.env.USER);
-console.log("PASSWORD:", process.env.PASSWORD );
-console.log("HOST:", process.env.HOST);
-
 const sequelize = new Sequelize(process.env.DBNAME || "", process.env.USER || "", process.env.PASSWORD || "", {
   host: process.env.HOST || "",
   port: 5432,
